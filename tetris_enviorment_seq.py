2eef57c01669b99c7393082bcebc2c0c9567f946
TheSlenk <stephenmamosian@gmail.com>
2026-08-10 21:10:18 -0400

    feat: Adding starting of seq env SM/Init

--- tetris_enviorment_seq.py
+++ tetris_enviorment_seq.py
@@ -1,73 +1,84 @@
-# import functools
+import functools
 
-# import gymnasium
-# import numpy as np
-# from gymnasium.spaces import Discrete
-# from gymnasium.utils import seeding
+import gymnasium
+import numpy as np
+from gymnasium.spaces import Discrete
+from gymnasium.utils import seeding
 
-# from pettingzoo import AECEnv
-# from pettingzoo.utils import AgentSelector, wrappers
+from pettingzoo import AECEnv
+from pettingzoo.utils import AgentSelector, wrappers
 
-# from tetris import Tetris
-# from tetris_game import NUM_DISTINCT_ACTIONS, ACTION_MAPPING, OBS_SHAPE
+from tetris_game import TetrisGame, NUM_DISTINCT_ACTIONS, OBS_SHAPE
 
-# def env(render_mode=None):
-#     internal_render_mode = render_mode if render_mode != "ansi" else "human"
-#     env = raw_env(render_mode=internal_render_mode)
+def env(render_mode=None)
+    internal_render_mode = render_mode if render_mode != "ansi" else "human"
+    env = raw_env(render_mode=internal_render_mode)
 
-#     if render_mode == "ansi":
-#         env = wrappers.CaptureStdoutWrapper(env)
+    if render_mode == "ansi":
+        env = wrappers.CaptureStdoutWrapper()
 
-#     env = wrappers.AssertOutOfBoundsWrapper(env)
-#     env = wrappers.OrderEnforcingWrapper(env)
-#     return env
+    env = wrappers.AssertOutOfBoundsWrapper(env)
 
-# class raw_env(AECEnv):
-#     metadata = {
-#         "render_mode": ["human"],
-#         "name": "tetris_v1"
-#     } 
 
-#     def __init__(self, render_mode=None):
-#         self.possible_agents = ["player_1"]
+    env = wrappers.OrderEnforcingWrapper(env)
+    return env
 
-#         self.env = None
-#         self._action_spaces = {agent: Discrete(NUM_DISTINCT_ACTIONS) for agent in self.possible_agents}
-#         self._observation_spaces = {
-#             agent: Discrete(OBS_SHAPE) for agent in self.possible_agents
-#         }
+class raw_env(AECEnv):
+    metadata = {"render_modes": ["human"], "name": "tetris_v1"}
 
-#         self.render_mode = render_mode
+    def __init__(self, render_mode=None):
+        self.possible_agents = ["player_1"]
 
-#     @functools.lru_cache(maxsize=None)
-#     def observation_space(self, agent):
-#         return Discrete(OBS_SHAPE)
-    
-#     @functools.lru_cache(maxsize=None)
-#     def action_space(self, agent):
-#         return Discrete(NUM_DISTINCT_ACTIONS)
+        self.agent_name_mapping = dict(
+            zip(self.possible_agents, list(range(len(self.possible_agents))))
+        )
 
-#     def render(self):
-#         if self.render_mode is None:
-#             gymnasium.logger.warn(
-#                 "You are calling render method without specifying any render mode."
-#             )
+        self._action_spaces = {agent: Discrete(NUM_DISTINCT_ACTIONS) for agent in self.possible_agents}
+        self._observation_spaces = {
+            agent: Discrete(OBS_SHAPE) for agent in self.possible_agents
+        }
+        self.render_mode = render_mode
 
-#             return
+    @functools.lru_cache(maxsize=None)
+    def observation_space(self, agent):
+        return Discrete(OBS_SHAPE)
 
-#         if self.env.done:
-#             print("Gameover")
-#         else:
-#             print(self.env)
-    
-#     def observe(self, agent):
-#         return np.array(self.observation_spaces[agent])
-    
-#     def close(self):
-#         pass
+    @functools.lru_cache(maxsize=None)
+    def action_space(self, agent):
+        return Discrete(NUM_DISTINCT_ACTIONS, seed=self.np_random_seed)
 
-#     def reset(self, seed = None, options = None):
-#         self.agents = self.possible_agents[:]
-#         self.rewards = {agent: 0 for agent in self.agents}
+    def render(self):
 
-# TODO: Complete reset and step
+        if self.render_mode is None:
+            gymnasium.logger.warn(
+                "You are calling render method without specifying any render mode."
+            )
+            return
+
+        if len(self.agents) == 1:
+            string = "Current state: Agent1: {}".format(
+                self.state[self.agents[1]]
+            )
+        else:
+            string = "Game over"
+        print(string)
+
+    def observe(self, agent):
+        return np.array(self.observations[agent])
+
+    def reset(self, seed = None, options = None):
+
+        if seed is not None:
+            self.np_random, self.np_random_seed = seeding.np_random(seed)
+        self.agents = self.possible_agents[:]
+        self.rewards = {agent: 0 for agent in self.agents}
+        self._cumulative_rewards = {agent: 0 for agent in self.agents}
+        self.terminations = {agent: False for agent in self.agents}
+        self.truncations = {agent: False for agent in self.agents}
+        self.infos = {agent: {} for agent in self.agents}
+        self.state = {agent: None for agent in self.agents}
+        self.observations = {agent: None for agent in self.agents}
+        self.num_moves = 0
+
+        self._agent_selector = AgentSelector(self.agents)
+        self.agent_selection = self._agent_selector.next()
